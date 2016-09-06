4a108344255f7b060732dbd5a27b8b5a5fc41b33
Will Davies <william.davies@thinksolid.com>
2016-09-06 14:35:35 -0700

    first commit

---
+++ src/app/pages/ui/components/icons/icons.component.ts
@@ -0,0 +1,22 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+import {IconsService} from './icons.service';
+
+@Component({
+  selector: 'icons',
+  encapsulation: ViewEncapsulation.None,
+  providers: [IconsService],
+  styles: [require('./icons.scss')],
+  template: require('./icons.html'),
+})
+export class Icons {
+
+  icons:any;
+
+  constructor(private _iconsService: IconsService) {
+  }
+
+  ngOnInit() {
+    this.icons = this._iconsService.getAll();
+  }
+}
